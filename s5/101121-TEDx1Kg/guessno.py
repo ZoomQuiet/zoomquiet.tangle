204161be4ab8b5729cec7765d6cb2c0714025eba
Zoom.Quiet <zoomquiet+hg@gmail.com>
2010-11-21 12:22:18 +0800

    ??zhpy script

--- s5/101121-TEDx1Kg/guessno.py
+++ s5/101121-TEDx1Kg/guessno.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from random import uniform
-
 myNumber = int(uniform(2,99))
 noGuess = 1
 while noGuess:
     urNumber = int(raw_input('猜个1~99的数字? --> '))
-    if urNumber < myNumber:
-        print "...(~_~)小了"
-    elif urNumber > myNumber:
-        print "......大了(^_-"
+    if urNumber < myNumber: print "...(~_~)小了"
+    elif urNumber > myNumber: print "......大了(^_-"
     else:
         print "猜对了\(^o^)/下次再来玩!"
         break
