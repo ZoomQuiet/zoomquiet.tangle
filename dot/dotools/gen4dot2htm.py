792d3e7c1baa8ffba56d542b11f1ad0a165ea869
Zoom.Quiet <zoomquiet+hg@gmail.com>
2012-03-15 17:13:42 +0800

    cancel stickyfooter.css

--- dot/dotools/gen4dot2htm.py
+++ dot/dotools/gen4dot2htm.py
@@ -26,11 +26,6 @@ DEFEXPORT = "stdout"
 DEFTITLE = "mapping base .dot"
 MAPID = "SomeGraph"
 DOTXT = "%s.dot"%MAPID
-#<title>%(pageTitle)s</title>
-#    <div id="%(mainMap)s"/>
-#    %(mainImageMaps)s
-#    <div id="%(mapLegend)s"/>
-#   %(styleData)s
 TPLidxHTM='''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN" lang="zh-CN">
 <head>
